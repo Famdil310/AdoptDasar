6769b3f67a44d9478ab1f36c391516d035f40d49
Famdil310 <farouqeldian@gmail.com>
2021-03-21 15:47:13 +0700

    2.3 Enkapsulasi

---
+++ lib/persegi_panjang.dart
@@ -0,0 +1,25 @@
+class PersegiPanjang{
+  double _panjang;
+  double _lebar;
+
+  void set lebar(double value) {
+    _lebar = value;
+  }
+
+  void setPanjang(double value){
+    if(value < 0){
+      value *= -1;
+    }
+    _panjang = value;
+  }
+
+  double getPanjang() {
+    return _panjang;
+  }
+
+  double hitungLuas(){
+    return this._panjang * _lebar;
+  }
+
+  double get luas => _panjang * _lebar;
+}
