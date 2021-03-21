6ac12f342ed8318646bc3a69230a0f9dce37a19c
Famdil310 <farouqeldian@gmail.com>
2021-03-21 15:17:20 +0700

    2.2 Class & Object

---
+++ bin/pbo_ClassdanObject.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+main(List<String> arguments) {
+  PersegiPanjang kotak1, kotak2;
+  double luaskotak1;
+
+  kotak1 = new PersegiPanjang();
+  kotak1.panjang = 2;
+  kotak1.lebar = 3;
+
+  kotak2 = PersegiPanjang();
+  kotak2.panjang = double.tryParse(stdin.readLineSync());
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+  luasKotak1 = kotak1.hitungLuas();
+
+  print(kotak1 + kotak2.hitungLuas());
+}
+
+class PersegiPanjang{
+  double panjang;
+  double lebar;
+}
